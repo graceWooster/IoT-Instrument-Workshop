976781a00f1d8917a973e29ff3df0a3e3d8a87da
Grace Wu <143197391+graceWooster@users.noreply.github.com>
2025-12-04 03:29:32 -0500

    Add files via upload
    Added first version of the code for the workshop

---
+++ InstrumentWorkshopGABC.ino
@@ -0,0 +1,45 @@
+int BUTTON_PINS[4] = {22, 23, 32, 33};
+#define BUZZER_PIN 25 // ESP32 GPIO21 pin connected to Buzzer's pin
+int frequencies[4] = {392, 440, 494, 523};   //second half G-A-B-C
+int buttonState[4] = {0, 0, 0, 0};
+
+void setup() {
+  Serial.begin(9600);                // initialize serial
+  pinMode(BUZZER_PIN, OUTPUT);       // set ESP32 pin to output mode
+  for (int i = 0; i < 4; i++) {
+    pinMode(BUTTON_PINS[i], INPUT_PULLUP);
+  }
+}
+
+void loop() {
+  for (int i = 0; i < 4; i++) {
+    buttonState[i] = digitalRead(BUTTON_PINS[i]);
+  }
+
+  while (buttonState[0] == LOW) {
+    Serial.print("1");
+      tone(BUZZER_PIN, frequencies[0]); // turn on
+      buttonState[0] = digitalRead(BUTTON_PINS[0]);
+  }
+
+  while (buttonState[1] == LOW) {
+    Serial.print("2");
+      tone(BUZZER_PIN, frequencies[1]); // turn on
+      buttonState[1] = digitalRead(BUTTON_PINS[1]);
+  }
+
+  while (buttonState[2] == LOW) {
+    Serial.print("3");
+      tone(BUZZER_PIN, frequencies[2]); // turn on
+      buttonState[2] = digitalRead(BUTTON_PINS[2]);
+  }
+
+  while (buttonState[3] == LOW) {
+    Serial.print("4");
+      tone(BUZZER_PIN, frequencies[3]); // turn on
+      buttonState[3] = digitalRead(BUTTON_PINS[3]);
+  }
+
+  noTone(BUZZER_PIN);  // turn off
+}
+
